5e95ae9fdd5bcb66fdcb5705b5513ae87170681e
angu0143 <angu0143@student.monash.edu>
2025-10-21 17:06:49 +1100

    trying to connect charts to dashbaord

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,8 +1,8 @@
-var vg_1 = "youthcrime_state_map_2022_2023.json"
+var vg_1 = "youthcrime_state_map_2022_2023.vg.json"
 vegaEmbed("#map_chart",vg_1).then(function(result) {
 }).catch(console.error);
 
 
-var vg_2 = "youth_offender_rate_age_sex.json"
+var vg_2 = "youth_offender_rate_age_sex.vg.json"
 vegaEmbed("#line_chart",vg_2).then(function(result) {
 }).catch(console.error);
