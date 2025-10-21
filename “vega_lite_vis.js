e938a39d16bd81a70559a428d57dbe548f1f6763
angu0143 <angu0143@student.monash.edu>
2025-10-21 16:40:24 +1100

    connecting

--- “vega_lite_vis.js
+++ “vega_lite_vis.js
@@ -1,3 +1,8 @@
 var vg_1 = "youthcrime_state_map_2022_2023.json"
-vegaEmbed("#map",vg_1).then(function(result) {
+vegaEmbed("#map_chart",vg_1).then(function(result) {
+}).catch(console.error);
+
+
+var vg_2 = "youth_offender_rate_age_sex.json"
+vegaEmbed("#line_chart",vg_2).then(function(result) {
 }).catch(console.error);
